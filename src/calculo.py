9c4cbf63628388ed39fa98cd2d636c7099cad796
Alison Rian <alisonrianbr@gmail.com>
2025-01-22 19:55:58 -0300

    Implementa a função principal para calcular juros-compostos

--- src/calculo.py
+++ src/calculo.py
@@ -1,10 +1,16 @@
 opc = 0
 while opc != 1:
-    capitalInicial = float(input('Adicione o valor do capital inicial:'))
-    taxa = float(input('Informe a taxa de juros:'))
-    tempo = int(input('Informe o tempo(meses):'))
-    montante = capitalInicial * (1+taxa)**tempo
-    print(montante)
+    try:
+        print("Funcionalidade principal")
+        capital = float(input("Digite o capital inicial (C): "))
+        taxa = float(input("Digite a taxa de juros (em %): ")) / 100
+        tempo = int(input("Digite o tempo em períodos (t): "))
+
+        montante = capital * (1 + taxa) ** tempo
+        print(f"O montante após {tempo} períodos será de R$ {montante:.2f}.")
+    except ValueError:
+        print("Por favor, insira valores válidos.")
+
     opc = int(input("Digite 0 para continuar e 1 para sair"))
 
 print('Encerrando...')
